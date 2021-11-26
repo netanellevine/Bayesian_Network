03351b43b3af0208625fc14d56ef38275097a765
Netanel <netanellevine123@gmail.com>
2021-11-27 00:10:56 +0200

    finished the code. arranging and making notes

--- src/Network.java
+++ src/Network.java
@@ -1,20 +1,25 @@
 import java.util.ArrayList;
 
 public class Network {
-    private ArrayList<Variable> net;
+    private final ArrayList<Variable> net;
 
     public Network() {
         this.net = new ArrayList<>();
     }
 
-    public Network(ArrayList<Variable> net) {
-        this.net = net;
-    }
-
+    /**
+     * This method adds a new Variable to the Network.
+     * @param var - Variable var.
+     */
     public void add_variable(Variable var){
         this.net.add(var);
     }
 
+    /**
+     * This method checks whether var_name given is belongs to the Network as Variable.
+     * @param var_name - String var_name.
+     * @return - true if the Network contains, false otherwise.
+     */
     public boolean isVariable(String var_name){
         for (Variable variable : this.net) {
             if (variable.getVar_name().equals(var_name)) {
@@ -24,6 +29,11 @@ public class Network {
         return false;
     }
 
+    /**
+     * This method receives a String var_name and returns a Variable from this Network with the name of var_name.
+     * @param var_name - String var_name.
+     * @return - Variable from the Network, returns null can't find the variable.
+     */
     public Variable getVariable(String var_name){
         if(isVariable(var_name)) {
             for (Variable variable : this.net) {
@@ -32,24 +42,39 @@ public class Network {
                 }
             }
         }
-            String[] a = {};
            return null;
     }
 
+    /**
+     * This method resets all the Evidence of each Variable in the Network.
+     * It uses the method resetEvidence() from the Variable class.
+     */
     public void resetEvidence(){
         for (Variable variable : this.net) {
             variable.resetEvidence();
         }
     }
 
+    /**
+     * This method returns the Network.
+     * @return - Network net.
+     */
     public ArrayList<Variable> getNet() {
         return this.net;
     }
 
+    /**
+     * This method removes a Variable from the Network.
+     * @param var - Variable var.
+     */
     public void removeVariable(Variable var){
         this.net.remove(var);
     }
 
+    /**
+     * This method Override Object.toString()
+     * @return - String of the Network.
+     */
     @Override
     public String toString(){
         String out = "";
