3b8a4df80f5184ee400b29e1580ed41f223484e8
netanel <netanellevine123@gmail.com>
2022-06-03 11:04:29 +0300

    jar

--- src/Ex1.java
+++ src/Ex1.java
@@ -18,7 +18,7 @@ public class Ex1 {
     public static void writeToOutput(String file_name){
         // parse the file in the class inputReader which activates the class XmlParser for the Xml parsing.
         inputReader firstParse = new inputReader(file_name);
-        System.out.println( firstParse.getNet());
+//        System.out.println( firstParse.getNet());
         // all the Variable Elimination question.
         ArrayList<String> VEQ = firstParse.getVE_questions();
         // all the Bayes Ball questions.
@@ -27,7 +27,9 @@ public class Ex1 {
         // go over on all the Bayes Ball questions.
         for (String item : BBQ) {
             Network net = firstParse.getNet();
-            output += (BayesBallAlgo.Bayes(net, item)) + "\n";
+            String curr_ans = BayesBallAlgo.Bayes(net, item);
+            output += (curr_ans) + "\n";
+            System.out.println(item + " -> " + curr_ans);
         }
         // go over on all the Variable Elimination questions.
         for (String value : VEQ) {
@@ -35,13 +37,14 @@ public class Ex1 {
             Network net = parser.getNet();
             VariableElimination VEA = new VariableElimination(value, net);
             output += (VEA.getAnswer() + "," + VEA.getAdd_actions() + "," + VEA.getMultiply_actions()) + "\n";
+            System.out.println(value + " -> answer=" + VEA.getAnswer() + ", total add actions=" + VEA.getAdd_actions() + ", total multiply actions=" + VEA.getMultiply_actions());
         }
         try {
             // write to output.
             FileWriter myWriter = new FileWriter("output.txt");
             myWriter.write(output);
             myWriter.close();
-            System.out.println("Successfully wrote to the file.");
+            System.out.println("\nSuccessfully wrote to the file.");
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
